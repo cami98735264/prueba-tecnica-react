e1f965d98a579352fb90b46d6385eedd8f2e3387
cami98735264 <cr7montealegre2010@gmail.com>
2025-04-30 00:36:27 -0500

    Refactor AuditoryCard component to centralize icon styling; replace width and height props with style object for consistency.

--- src/components/Cards/AuditoryCard.tsx
+++ src/components/Cards/AuditoryCard.tsx
@@ -20,15 +20,20 @@ const AuditoryCard = ({ icon, description }: AuditoryCardProps) => {
     },
     auditoryContent: {
       flex: 1,
+
       alignItems: "center",
       display: "flex",
       gap: "8px",
     },
+    icon: {
+      minWidth: "26px",
+      minHeight: "26px",
+    }
   };
   return (
     <div className="auditory-card" style={styles.auditory}>
       <div className="auditory-card__content" style={styles.auditoryContent}>
-        <Icon icon={icon} width="26" height="26" color={color.secondary} />
+        <Icon icon={icon} color={color.secondary} style={styles.icon} />
         <h3
           className="auditory-card__description"
           style={styles.description}
